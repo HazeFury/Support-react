e013b0cf897bca525c60d3e4edc50cad75eb4e94
HazeFury <mab.dev69@gmail.com>
2024-03-29 11:54:35 +0100

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import Name from "./components/Name";
-import Vehicule from "./components/vehicule";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const vehicules = [
-    {
-      marque: "BMW",
-      model: "M5",
-      couleur: "gris",
-      année: "2024",
-    },
-    {
-      marque: "Audi",
-      model: "RS6",
-      couleur: "noir",
-      année: "2023",
-    },
-    {
-      marque: "Mercedes",
-      model: "Classe AMG",
-      couleur: "blanc",
-      année: "2022",
-    },
-  ];
+  // ETAPE 1 : j'initialise mes states avec une valeur par défaut
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [switchMode, setSwitchMode] = useState(false);
+
+  console.log(switchMode);
+
+  // ETAPE 2 : je crée les fonctions pour modifier la valeur de mes states (AVEC LE SETTER (ex : setCount))
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+  // function handleAdd() {
+  //   setCount(count + 1);
+  // }
+  const handleEditText = (e) => {
+    setText(e.target.value);
+  };
+  const handleSwitch = () => {
+    setSwitchMode(!switchMode);
+  };
 
   return (
-    <div>
-      {vehicules.map((vehicule) => (
-        <Vehicule informations={vehicule} />
-      ))}
+    <div className="container">
+      {/* ETAPE 3 : je les affiche */}
+      <h1>{count}</h1>
+      <p>{text}</p>
+      {/* ETAPE 4 : dans la propriété onClick ou onChange, j'appelle la fonction correspondante  */}
+      <button onClick={handleAdd}>ajouter 1</button>
+      <input type="text" value={text} onChange={handleEditText} />
+      <button onClick={handleSwitch}>switcher entre true et false</button>
     </div>
   );
 }
